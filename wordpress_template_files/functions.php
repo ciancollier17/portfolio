ae225c91db579ae8b83bf1b510127715ef46c0da
Cian Collier <ciancollier17@gmail.com>
2019-09-24 19:29:24 +0100

    Displayed project gallery and about section in index.php

--- wordpress_template_files/functions.php
+++ wordpress_template_files/functions.php
@@ -9,5 +9,37 @@
     wp_enqueue_style('CianCollierFonts');
   }
 
+  function ciancollier_theme_settings() {
+    echo "<div class='wrap'>";
+    echo "<h1>Cian Collier Theme Settings</h1>";
+    echo "<form method='post' action='options.php'>";
+    settings_fields('ciancollier-theme');
+    do_settings_sections('ciancollier-theme');
+    submit_button();
+    echo "</form>";
+    echo "</div>";
+  }
+
+  function ciancollier_admin_menu() {
+    add_theme_page('Cian Collier Theme Settings', 'Theme Settings', 'manage_options', 'ciancollier-theme-menu', 'ciancollier_theme_settings');
+  }
+
+  function ciancollier_register_settings() {
+    register_setting('ciancollier-theme', 'about-section');
+
+    add_settings_section('about-settings-section', 'About Me Settings', null, 'ciancollier-theme');
+    add_settings_field('about', 'About Me Text', 'about_callback', 'ciancollier-theme', 'about-settings-section');
+  }
+
+  function about_callback() {
+    $current_value = get_option('about-section');
+    echo "<textarea id='about' name='about-section'>" . $current_value . "</textarea>";
+  }
+
   add_action('wp_enqueue_scripts', 'ciancollier_get_scripts');
+  add_action('admin_menu', 'ciancollier_admin_menu');
+  add_action('admin_init', 'ciancollier_register_settings');
+
+  // Add support for thumbnails for displaying them on the project gallery
+  add_theme_support('post-thumbnails'); 
 ?>
--- wordpress_template_files/index.php
+++ wordpress_template_files/index.php
@@ -1,3 +1,38 @@
 <?php get_header(); ?>
 
+<div class="content" id="projects">
+  <div class="coloured">
+    <div id="project-gallery">
+      <div id="project-container">
+        <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+          <div class="project">
+            <?php echo "<img src=\"" . get_the_post_thumbnail_url() . "\" alt=\"Screenshot 1\">"; ?>
+            <div id="pt1" class="project-title">
+              <h3><?php the_title(); ?></h3>
+              <?php echo "<a href=\"" . get_the_permalink() . "\"><i class=\"far fa-eye\"></i> View Project</a>"; ?>
+              <?php echo "<a href=\"" . get_post_meta(get_the_ID(), 'github', true) . "\" target=\"_blank\"><i class=\"fab fa-github\"></i> View Source</a>"; ?>
+            </div>
+          </div>
+        <?php endwhile; else : ?>
+	         <p><?php esc_html_e( 'Sorry, no posts matched your criteria.' ); ?></p>
+         <?php endif; ?>
+      </div>
+      <div id="pagination">
+        <?php echo paginate_links(array(
+          "prev_text" => "<span class=\"page arrow\"><i class=\"fas fa-angle-double-left\"></i></span>",
+          "next_text" => "<span class=\"page arrow\"><i class=\"fas fa-angle-double-right\"></i></span>",
+          "before_page_number" => "<span class=\"page\">",
+          "after_page_number" => "</span>"
+        )); ?>
+      </div>
+    </div>
+  </div>
+  <div class="white">
+    <div>
+      <h2 class="section-title">About Me</h2>
+      <?php echo get_option('about-section'); ?>
+    </div>
+  </div>
+</div>
+
 <?php get_footer(); ?>
