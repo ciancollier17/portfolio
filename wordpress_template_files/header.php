ff6bd2cb7485056a28d650319b8cd602e5d28480
Cian Collier <ciancollier17@gmail.com>
2019-09-25 16:14:07 +0100

    Set up single.php template file

--- wordpress_template_files/header.php
+++ wordpress_template_files/header.php
@@ -2,25 +2,19 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title><?php bloginfo('name'); ?></title>
+    <?php
+      function get_page_title() {
+        $title = bloginfo('name');
+
+        if (is_single()) {
+          $title = $title . single_post_title(": ", true);
+        }
+
+        return $title;
+      }
+    ?>
+    <title><?php get_page_title(); ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php wp_head(); ?>
   </head>
   <body>
-    <div id="hero">
-      <div id="background-img"></div>
-      <div class="half coloured">
-        <div>
-          <h1><?php bloginfo('name'); ?></h1>
-          <h2><?php bloginfo('description'); ?></h2>
-          <button id="button-projects"><i class="far fa-arrow-alt-circle-down"></i><br />My Work</button>
-        </div>
-      </div>
-      <div class="half white">
-        <div>
-          <h1><?php bloginfo('name'); ?></h1>
-          <h2><?php bloginfo('description'); ?></h2>
-          <button id="button-contact"><i class="far fa-handshake"></i><br />Hire Me</button>
-        </div>
-      </div>
-    </div>
--- wordpress_template_files/index.php
+++ wordpress_template_files/index.php
@@ -1,5 +1,23 @@
 <?php get_header(); ?>
 
+<div id="hero">
+  <div id="background-img"></div>
+  <div class="half coloured">
+    <div>
+      <h1><?php bloginfo('name'); ?></h1>
+      <h2><?php bloginfo('description'); ?></h2>
+      <button id="button-projects"><i class="far fa-arrow-alt-circle-down"></i><br />My Work</button>
+    </div>
+  </div>
+  <div class="half white">
+    <div>
+      <h1><?php bloginfo('name'); ?></h1>
+      <h2><?php bloginfo('description'); ?></h2>
+      <button id="button-contact"><i class="far fa-handshake"></i><br />Hire Me</button>
+    </div>
+  </div>
+</div>
+
 <div class="content" id="projects">
   <div class="coloured">
     <div id="project-gallery">
