73a372597e7cca6201b7f5e2f88be20342ea09ff
Cian Collier <ciancollier17@gmail.com>
2019-09-25 17:33:44 +0100

    Added content to single.php template

--- wordpress_template_files/single.php
+++ wordpress_template_files/single.php
@@ -1,3 +1,25 @@
 <?php get_header(); ?>
 
+<div class="content-project">
+  <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+    <h1><?php the_title(); ?></h1>
+    <i><?php the_author(); echo " on "; the_date(); ?></i>
+    <br />
+    <div id = "tags">
+    <?php
+      $tags = get_the_tags();
+      if ($tags) {
+        foreach ($tags as $tag) {
+          echo "<div class=\"tag\"><a href=\"#\">" . $tag->name . "</a></div>";
+        }
+      }
+    ?>
+    </div>
+    <?php the_content(); ?>
+  <?php endwhile; else : ?>
+   <p><?php esc_html_e( 'Sorry, no posts matched your criteria.' ); ?></p>
+  <?php endif; ?>
+</div>
+</div>
+
 <?php get_footer(); ?>
