8f8ae616fb0940353e48e6e22349ab20eff4eb67
Cian Collier <ciancollier17@gmail.com>
2019-09-25 18:39:38 +0100

    Styled single.php headings and tag list

--- wordpress_template_files/single.php
+++ wordpress_template_files/single.php
@@ -3,7 +3,7 @@
 <div class="content-project">
   <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
     <h1><?php the_title(); ?></h1>
-    <i><?php the_author(); echo " on "; the_date(); ?></i>
+    <i><?php echo "<a href=\"#\">"; the_author(); echo "</a> on <a href=\"#\">"; the_date(); echo "</a>"; ?></i>
     <br />
     <div id = "tags">
     <?php
