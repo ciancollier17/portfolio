6d1dcef53ab5f417565b1dfbeb04427d22531323
Cian Collier <ciancollier17@gmail.com>
2019-09-26 16:44:41 +0100

    Completed styling for single.php

--- wordpress_template_files/single.php
+++ wordpress_template_files/single.php
@@ -15,6 +15,27 @@
       }
     ?>
     </div>
+    <?php echo "<a id=\"home-button-link\" href=\"" . get_home_url() . "\">"; ?>
+    <i class="fas fa-home"></i>
+    <?php echo "</a>"; ?>
+    <div class="featured-img">
+      <?php the_post_thumbnail(); ?>
+      <div class="buttons">
+        <?php
+          $livedemo = get_post_meta(get_the_ID(), 'live-demo', true);
+
+          if ($livedemo) {
+            echo "<a href=\"" . $livedemo . "\"><i class=\"fas fa-laptop-code\"></i> Live Demo</button></a>";
+          }
+
+          $github = get_post_meta(get_the_ID(), 'github', true);
+
+          if ($github) {
+            echo "<a href=\"" . $github . "\"><i class=\"fab fa-github\"></i> View Code</button></a>";
+          }
+        ?>
+      </div>
+    </div>
     <?php the_content(); ?>
   <?php endwhile; else : ?>
    <p><?php esc_html_e( 'Sorry, no posts matched your criteria.' ); ?></p>
